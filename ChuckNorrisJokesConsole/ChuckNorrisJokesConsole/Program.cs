765b59f43654e26833722c425961f6c8e62eb093
Lusanda <3444709@myuwc.ac.za>
2019-10-02 15:12:08 +0200

    initial chucknorris commit

---
+++ ChuckNorrisJokesConsole/ChuckNorrisJokesConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using ChuckNorrisJokesLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokesConsole
+{
+    class Program
+    {
+        static async  Task Main(string[] args)
+
+        {
+            JokeGenerator jokeGenerator = new JokeGenerator();
+            string[] randomJoke = await jokeGenerator.Categories();
+
+            Console.WriteLine(randomJoke);
+
+            string[] categories = await jokeGenerator.Categories();
+            for (int index = 0; index < categories.Length; index++)
+        }
+    }
+}
